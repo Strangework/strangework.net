402161b5e7552512fab9708980aaa54c89710a9d
Strangework <nicholas.yang.chee@gmail.com>
2018-04-09 01:10:43 +0000

    Working prototype for Spotify playlist commenting app

---
+++ strangework.py
@@ -0,0 +1,183 @@
+import flask
+import os
+import requests 
+import urllib.parse
+import sqlite3
+
+
+app = flask.Flask(__name__)
+
+
+# Generate secret key for encrypting cookies and session data
+app.secret_key = os.urandom(24)
+
+
+# URLs and endpoints
+SPOTIFY_AUTH_URL = 'https://accounts.spotify.com/authorize'
+SPOTIFY_TOKEN_URL = 'https://accounts.spotify.com/api/token'
+
+AUTH_REDIRECT_URI = 'http://strangework.net/xee/callback'
+
+
+# Spotify app information
+SPOTIFY_CLIENT_ID = '1d94080f30a04f85a93a711d8596784b'
+SPOTIFY_CLIENT_SECRET = 'c7894c618092493b9b352737987e2a21'
+
+
+# Comment database file
+XEE_COMMENTS_DB = 'xee.sqlite3'
+
+@app.route('/')
+def ssd_harness():
+  return flask.render_template('ssd_harness.html')
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+  return flask.render_template('hello.html', name=name)
+
+@app.route('/xee')
+def xee_login():
+  auth_query_params = {
+    'client_id': SPOTIFY_CLIENT_ID,
+    'scope': 'playlist-read-collaborative',
+    'response_type': 'code',
+    'redirect_uri': AUTH_REDIRECT_URI
+  }
+  url_args = "&".join(["{}={}".format(key,urllib.parse.quote(val)) for key,val in auth_query_params.items()])
+  auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+  return flask.redirect(auth_url)
+
+@app.route('/xee/callback')
+def xee():
+  # Pull authentication token from incoming request
+  auth_token = flask.request.args['code']
+  token_payload = {
+    'grant_type': 'authorization_code',
+    'code': auth_token,
+    'redirect_uri': AUTH_REDIRECT_URI,
+    'client_id': SPOTIFY_CLIENT_ID,
+    'client_secret': SPOTIFY_CLIENT_SECRET
+  }
+  token_resp = requests.post(SPOTIFY_TOKEN_URL, data=token_payload)
+
+  # Receive authorization token
+  token_data = token_resp.json() 
+  access_token = token_data['access_token']
+  refresh_token = token_data['refresh_token']
+  token_type = token_data['token_type']
+  expires_in = token_data['expires_in']
+
+  # Build authorization header
+  authorization_header = {"Authorization":"Bearer {}".format(access_token)}
+
+  # Get user information
+  spotify_profile_url = 'https://api.spotify.com/v1/me'
+  profile_resp  = requests.get(spotify_profile_url, headers=authorization_header)
+  profile_data  = profile_resp.json()
+  user_id = profile_data['id']
+  profile_url = profile_data['href']
+
+  # Store user's Spotify user ID in the session
+  flask.session['spotify_user_id'] = user_id
+
+  # Get user's collaborative playlists
+  playlist_query_resp = requests.get('{}/playlists'.format(profile_url), headers=authorization_header)
+  playlist_query_data = playlist_query_resp.json()
+  # Currently only pulls up (x)ee
+  playlist_names = ''
+  playlist_url = ''
+  for item in playlist_query_data['items']:
+    if item['collaborative'] and item['name'] == '(x)ee':
+      playlist_names += item['name'] + " "
+      playlist_url = item['href']
+      print("HEY! " +playlist_url)
+
+  # Get playlist object
+  playlist_resp = requests.get(playlist_url, headers=authorization_header)
+  playlist_data = playlist_resp.json()
+  print(playlist_data)
+
+  # Track storage format
+  # [
+  # {
+  #   "title":"",
+  #   "artist":"",
+  #   "art_url":"",
+  #   "id":""
+  # }
+  # ]
+  tracks = []
+  for track in playlist_data['tracks']['items']:
+    curr_track = {}
+    curr_track['title'] = track['track']['name']
+    curr_track['artist'] = track['track']['artists'][0]['name']
+    curr_track['art_url'] = track['track']['album']['images'][0]['url']
+    curr_track['id'] = track['track']['id']
+    tracks.append(curr_track)
+  
+  return flask.render_template('xee.html', spotify_name=user_id, tracks=tracks, playlist_names=playlist_names)
+  
+# Returns an array of comments for the provided song in the structure below
+# [
+# {
+#   "user":"",
+#   "comment:"",
+# }
+# ]
+@app.route('/xee/song/<id>')
+def get_song_comments(id):
+  #comments = []
+  #first_comment = {
+  #  "user":"Me",
+  #  "comment":"Ah! Song " +id+ "! This is a good song!"
+  #}
+  #second_comment = {
+  #  "user":"Myself",
+  #  "comment":"Yes! This a good song!"
+  #}
+  #comments.append(first_comment)
+  #comments.append(second_comment)
+  
+  # Pull relevant comments from DB
+  conn = sqlite3.connect(XEE_COMMENTS_DB)
+  c = conn.cursor()
+  c.execute('select * from comments where id=?', (id,))
+  conn.commit()
+  
+  raw_comments = c.fetchall()
+  comments = []
+  for raw_comment in raw_comments:
+    comment = {}
+    comment['user'] = raw_comment[1]
+    comment['comment'] = raw_comment[2]
+    comments.append(comment)
+
+  print(comments)
+
+  return(flask.jsonify(comments))
+
+# Accepts a JSON object describing a comment in the structure below
+# {
+#   "song_id":"",
+#   "comment":""
+# }
+@app.route('/xee/comment', methods=['POST'])
+def write_comment():
+  comment_obj = flask.request.get_json(force=True)
+  # !! : Should check for login
+  print(flask.session['spotify_user_id'])
+  print(comment_obj)
+
+  # Write comment to DB
+  conn = sqlite3.connect(XEE_COMMENTS_DB)
+  c = conn.cursor()
+  params = (comment_obj['song_id'], flask.session['spotify_user_id'], comment_obj['comment'])
+  c.execute('insert into comments values(?,?,?)', params)
+  conn.commit()
+
+  resp = { 'ok' : True }
+  return flask.jsonify(resp)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
